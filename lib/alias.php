bc31cdcc0df744cf0a9d9a07ce07850128d698b2
Sean Huber <shuber@huberry.com>
2009-03-14 13:42:15 -0700

    Add Object#alias_method_chain support

--- lib/alias.php
+++ lib/alias.php
@@ -6,6 +6,12 @@ abstract class AliasMethods {
         extend_method($this, get_class($this), $old_name, $new_name);
     }
     
+    function alias_method_chain($method, $with) {
+        $this->instance_extended_methods[$method.'_without_'.$with] = $this->instance_extended_methods[$method];
+        unset($this->instance_extended_methods[$method]);
+        extend_method($this, get_class($this), $method.'_with_'.$with, $method);
+    }
+    
 }
 
 extend('Object', 'AliasMethods');
