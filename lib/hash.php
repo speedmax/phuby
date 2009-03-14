17da44bdc643ee8d51455ba73af9eec8635f3ee1
Sean Huber <shuber@huberry.com>
2009-03-14 12:44:20 -0700

    Add H#update

--- lib/hash.php
+++ lib/hash.php
@@ -2,9 +2,15 @@
 
 abstract class HashMethods {
     
-    public function merge($hash) {
+    function merge($hash) {
         if (is_a($hash, 'H')) $hash = $hash->array;
         return $this->new_instance(array_merge($this->array, $hash));
+    }
+    
+    function update($hash) {
+        if (is_a($hash, 'H')) $hash = $hash->array;
+        $this->array = array_merge($this->array, $hash);
+        return $this;
     }
     
 }
