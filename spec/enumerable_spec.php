598590178a4f185b999beca6d85ed95066470996
Sean Huber <shuber@huberry.com>
2009-03-14 14:08:05 -0700

    Fix existing specs

--- spec/enumerable_spec.php
+++ spec/enumerable_spec.php
@@ -4,22 +4,18 @@ require 'spec_helper.php';
 class Describe_Enumerable extends SimpleSpec {
     
     function should_provide_native_array_interface() {
-        $e = new Enumerable;
+        $e = new A;
         
         #push
         $e[] = 'ing';
         $e[] = 'cool';
         $e[] = 'wow';
 
-        expects( $e->to_a() )->should_be(array('ing', 'cool', 'wow'));
-        
-        $e['name'] = 'taylor luk';
-        
-        expects($e['name'])->should_be('taylor luk');
+        expects( $e->array )->should_be(array('ing', 'cool', 'wow'));
     }
     
     function should_be_iteratable_with_foreach() {
-        $e = new Enumerable;
+        $e = new A;
         $e[] = 'ing';
         $e[] = 'cool';
         $e[] = 'wow';
@@ -33,7 +29,7 @@ class Describe_Enumerable extends SimpleSpec {
     }
     
     function should_provide_functional_iterators() {
-        $e = new Enumerable;
+        $e = new A;
         $e[] = 'ing';
         $e[] = 'cool';
         $e[] = 'wow';
