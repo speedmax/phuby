18712f1e1e11d464536369341f5dc122bc57cd30
Sean Huber <shuber@huberry.com>
2009-03-14 12:43:19 -0700

    Add H#merge

--- lib/hash.php
+++ lib/hash.php
@@ -1,7 +1,12 @@
 <?php
 
 abstract class HashMethods {
-    //
+    
+    public function merge($hash) {
+        if (is_a($hash, 'H')) $hash = $hash->array;
+        return $this->new_instance(array_merge($this->array, $hash));
+    }
+    
 }
 
 class H extends Enumerable { }
